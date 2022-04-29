e24e297f00a97a1d6ca9691afdb71e80a0175fb7
itshypax <me@hypax.wtf>
2022-04-29 16:13:13 +0200

    some progress

---
+++ assets/php/navigation.php
@@ -0,0 +1,10 @@
+<nav id="mainnav">
+    <ul>
+        <li><a href="#">Impressum</a></li>
+        <li><a href="ts3server://ts.3-stripes.de">TS3Connect</a></li>
+        <li><a href="#">Discord</a></li>
+        <li><a href="#">Regeln</a></li>
+        <li><a href="https://board.3-stripes.de/">Forum</a></li>
+        <a href="/"><img src="/assets/images/webp/Logo480.webp" alt="3-Stripes.de Logo" height="32px" width="auto"></a>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3-Stripes.de &raquo; GTA Roleplay</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/assets/images/ico/favicon.ico" type="image/x-icon">
+    <!-- Metas -->
+    <?php include 'assets/php/metas.php'; ?>
+    <!-- Load CSS -->
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <!--load all Font Awesome styles -->
+    <link href="/assets/fontawesome/css/all.css" rel="stylesheet">
+    <!-- Include Roboto font -->
+    <link href="/assets/fonts/roboto/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'assets/php/navigation.php'; ?>
+
+    <div class="main">
+        <div class="box bord">
+            <img src="/assets/images/webp/pageBG_plane.webp" width="100%" draggable="false" (dragstart)="false;" oncontextmenu="return false;" class="no-drag" style="height:100vh;width:100vw;" alt="">
+        </div>
+        <div class="centered">
+            <h1 class="heading_title">3-Stripes.de</h1>
+            <p class="heading_subtitle">Dein deutscher Roleplay Server!</p>
+        </div>
+    </div>
+</body>
+</html>
